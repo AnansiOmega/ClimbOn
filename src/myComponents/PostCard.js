721b186177686b028125e35a4d235e4f2c922ee7
AnansiOmega <ignasbutautas7@gmail.com>
2021-03-09 15:14:30 -0600

    working on displaying comments, and posts on the landing page, need to figure out an appealing way to display comments

--- src/myComponents/PostCard.js
+++ src/myComponents/PostCard.js
@@ -16,6 +16,9 @@ import FavoriteIcon from '@material-ui/icons/Favorite';
 import ShareIcon from '@material-ui/icons/Share';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
+import TextField from '@material-ui/core/TextField';
+import Button from "components/CustomButtons/Button.js";
+
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -29,6 +32,8 @@ const useStyles = makeStyles((theme) => ({
   expand: {
     marginLeft: 'auto',
   },
+  expandOpen: {
+  },
   avatar: {
     width: theme.spacing(7),
     height: theme.spacing(7)
@@ -37,9 +42,11 @@ const useStyles = makeStyles((theme) => ({
 
 export default function PostCard(props) {
   const classes = useStyles();
-  const {user_id, content } = props["post"]
+  const {user_id, content, id } = props["post"]
   const [expanded, setExpanded] = React.useState(false);
   const [poster, setPoster] = useState({})
+  const [comments, setComments] = useState([])
+  const [newComment, setNewComment] = useState('')
   const user = useSelector(state => state.user)
   useEffect(() => { // finds the friend associated with the post, so I can use their info to flourish the card
     if(!user.friends) return
@@ -49,9 +56,42 @@ export default function PostCard(props) {
   },[user])
 
 
-  const handleExpandClick = () => {
+  const handleShowComments = () => {
+    fetch(`http://localhost:3000/posts/${id}`)
+    .then(resp => resp.json())
+    .then(post => {
+      setComments(post.comments)
+    })
     setExpanded(!expanded);
   };
+
+  const handleSubmitNewComment = () => {
+    const user_id = localStorage.getItem('userId')
+    const reqObj = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ post_id: id, content: newComment, user_id})
+    }
+
+    fetch('http://localhost:3000/comments', reqObj)
+    .then(resp => resp.json())
+    .then(data => {
+      console.log(data)
+    })
+    setNewComment('')
+  }
+
+  const renderComments = () => {
+    return comments.map( comment => {
+      const commentImgUrl = `http://localhost:3000/${comment.user.photo}`
+    return <div className='comment-card'>
+      <Avatar className={classes.avatar} src={commentImgUrl}/>
+      <Typography paragraph>{comment.content}</Typography>
+      </div>
+  })
+  }
 
   const imgUrl = `http://localhost:3000/${poster.photo}`
   return (
@@ -76,38 +116,30 @@ export default function PostCard(props) {
           className={clsx(classes.expand, {
             [classes.expandOpen]: expanded,
           })}
-          onClick={handleExpandClick}
+          onClick={handleShowComments}
           aria-expanded={expanded}
-          aria-label="comment"
-        >
-          Comment
+          aria-label="show more"
+          >
+          <h4>Comments</h4>
+          <ExpandMoreIcon />
         </IconButton>
       </CardActions>
       <Collapse in={expanded} timeout="auto" unmountOnExit>
         <CardContent>
-          <Typography paragraph>Method:</Typography>
-          <Typography paragraph>
-            Heat 1/2 cup of the broth in a pot until simmering, add saffron and set aside for 10
-            minutes.
-          </Typography>
-          <Typography paragraph>
-            Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over medium-high
-            heat. Add chicken, shrimp and chorizo, and cook, stirring occasionally until lightly
-            browned, 6 to 8 minutes. Transfer shrimp to a large plate and set aside, leaving chicken
-            and chorizo in the pan. Add pimentón, bay leaves, garlic, tomatoes, onion, salt and
-            pepper, and cook, stirring often until thickened and fragrant, about 10 minutes. Add
-            saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
-          </Typography>
-          <Typography paragraph>
-            Add rice and stir very gently to distribute. Top with artichokes and peppers, and cook
-            without stirring, until most of the liquid is absorbed, 15 to 18 minutes. Reduce heat to
-            medium-low, add reserved shrimp and mussels, tucking them down into the rice, and cook
-            again without stirring, until mussels have opened and rice is just tender, 5 to 7
-            minutes more. (Discard any mussels that don’t open.)
-          </Typography>
-          <Typography>
-            Set aside off of the heat to let rest for 10 minutes, and then serve.
-          </Typography>
+          {renderComments()}
+          <TextField
+          label="Comment"
+          style={{ margin: 8 }}
+          placeholder="..."
+          fullWidth
+          margin="normal"
+          value={newComment}
+          onChange={e => setNewComment(e.target.value)}
+          InputLabelProps={{
+            shrink: true,
+          }}
+        />
+        <Button onClick={handleSubmitNewComment} round color='primary'>Comment</Button>
         </CardContent>
       </Collapse>
     </Card>
